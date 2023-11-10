116a0f6394574911c3ece7a0c034016b3f10585b
Madu Xavier <127335538+mariadxavier@users.noreply.github.com>
2023-11-10 15:31:10 -0300

    button dentro do form
    E alterações de tamanho no css do botao de cadastrar. Inicio das configurações de validação.

---
+++ frontend/js/validation-register.js
@@ -0,0 +1,4 @@
+const formRegister = document.getElementById('formRegister');
+const inputApelido = document.getElementById('div-apelido');
+const inputEmail = document.getElementById('div-email');
+const password = document.getElementById('div-password');
