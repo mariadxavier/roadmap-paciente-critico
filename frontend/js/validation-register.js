8478d7ab559263c88d2f37556c19d8558bbfd250
Madu Xavier <127335538+mariadxavier@users.noreply.github.com>
2023-11-10 17:36:57 -0300

    Teste span input validação

--- frontend/js/validation-register.js
+++ frontend/js/validation-register.js
@@ -1,4 +1,14 @@
-const formRegister = document.getElementById('formRegister');
-const inputApelido = document.getElementById('div-apelido');
-const inputEmail = document.getElementById('div-email');
-const password = document.getElementById('div-password');
+const formRegister = document.querySelector('#form-register');
+const valInputApelido = document.querySelector('#div-apelido');
+const valInputEmail = document.querySelector('#div-email');
+const valInputPassword = document.querySelector('#div-password');
+
+formRegister.addEventListener('', (event)=>{
+    event.preventDefault();
+
+    //Verificação - nome vazio:
+    if (valInputApelido.value === "" || valInputApelido.value === null) {
+        alert('Por favor, preencha seu nome');
+        return;
+    }
+})
