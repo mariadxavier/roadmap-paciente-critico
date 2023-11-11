1266d4462b32bf669ade9c062534f2c3ec25a488
ojpbraga <ojpbraga@gmail.com>
2023-11-11 17:33:58 -0300

    Progress Bar: estruturação e estilização

--- frontend/js/paginaPrincipal.js
+++ frontend/js/paginaPrincipal.js
@@ -3,3 +3,21 @@ const usuarioLogado = JSON.parse(jsonUsuario);
 const spanApelido = document.getElementById("user-apelido");
 
 spanApelido.textContent = usuarioLogado.apelido;
+
+// Teste-JP
+const header = document.getElementById('header');
+const progressBar = document.getElementById('progressBar');
+
+window.addEventListener('scroll', (event) => {
+    
+    console.log(scrollY);
+
+    if (scrollY => 40) {
+        header.style.display = "none";
+        progressBar.style.display = "flex";
+    } 
+    if (scrollY <= 39) {
+        header.style.display = "flex";
+        progressBar.style.display = "none";
+    }
+});
