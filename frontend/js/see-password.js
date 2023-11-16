cb7cc1ba49b99f185105e0d5a872e00d0b61a274
mariadxavier <mariadudaxavier7@gmail.com>
2023-11-16 16:50:16 -0300

    feat ver senha para login

--- frontend/js/see-password.js
+++ frontend/js/see-password.js
@@ -1,23 +1,47 @@
 const openEye = document.querySelector("#div-img-eye");
 const closedEye = document.querySelector("#div-img-eye-slash");
-const passwordText = document.querySelector("#div-password");
+const passwordTextReg = document.querySelector("#div-password");
+const passwordTextLog = document.querySelector("#div-input-password");
 
+
+// pagina de cadastro:
 openEye.addEventListener('click', ()=> {
-      if (passwordText.getAttribute('type') == 'password') {
-            passwordText.setAttribute('type', 'text');
+      if (passwordTextReg.getAttribute('type') == 'password') {
+            passwordTextReg.setAttribute('type', 'text');
             closedEye.style.display = 'block';
             openEye.style.display = 'none';
       } else {
-            passwordText.setAttribute('type', 'password');
+            passwordTextReg.setAttribute('type', 'password');
       }
 });
 
 closedEye.addEventListener('click', ()=> {
-      if (passwordText.getAttribute('type') == 'text') {
-            passwordText.setAttribute('type', 'password');
+      if (passwordTextReg.getAttribute('type') == 'text') {
+            passwordTextReg.setAttribute('type', 'password');
             openEye.style.display = 'block';
             closedEye.style.display = 'none';
       } else {
-            passwordText.setAttribute('type', 'password');
+            passwordTextReg.setAttribute('type', 'password');
+      }
+});
+
+// página de login:
+openEye.addEventListener('click', ()=> {
+      if (passwordTextLog.getAttribute('type') == 'password') {
+            passwordTextLog.setAttribute('type', 'text');
+            closedEye.style.display = 'block';
+            openEye.style.display = 'none';
+      } else {
+            passwordTextLog.setAttribute('type', 'password');
+      }
+});
+
+closedEye.addEventListener('click', ()=> {
+      if (passwordTextLog.getAttribute('type') == 'text') {
+            passwordTextLog.setAttribute('type', 'password');
+            openEye.style.display = 'block';
+            closedEye.style.display = 'none';
+      } else {
+            passwordTextLog.setAttribute('type', 'password');
       }
 });
