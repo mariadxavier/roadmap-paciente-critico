ded7fd3c4a9ab76e40f8b6200786667c8094244e
mariadxavier <mariadudaxavier7@gmail.com>
2023-11-16 14:14:13 -0300

    style lesson margin top conteudo

--- frontend/js/animation-lesson.js
+++ frontend/js/animation-lesson.js
@@ -22,7 +22,7 @@ window.addEventListener('scroll', function () {
         header.style.width = '100%';
         header.style.height = '8vh';
         header.style.borderRadius = '0 0 40px 40px';
-        main.style.marginTop = '60vh';
+        main.style.marginTop = '56.5vh';
         
     } else {
         img.style.display = 'block';
