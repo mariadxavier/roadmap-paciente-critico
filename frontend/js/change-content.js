8d672c8bda5fc1abc1bc745fd0495cb9b927db71
Madu Xavier <mariadudaxavier7@gmail.com>
2023-11-17 14:33:38 -0300

    TEST primeiro conteudo

--- frontend/js/change-content.js
+++ frontend/js/change-content.js
@@ -1,6 +1,6 @@
 import json from "./json/content.json" assert {type: 'json'}
 
-const teste = json;
+const content = json;
 
 const imgHeader = document.querySelector('#header-img-theme');
 const titleHeader = document.querySelector('#header-h1-title');
@@ -13,32 +13,11 @@ const mainVideo = document.querySelector('#main-video-1');
 const mainUl = document.querySelector('#main-ul-complement');
 const btnNext = document.querySelector('#main-btn-nextLesson');
 
-console.log(teste[0].unity1_fase1.title);
-
-titleHeader.textContent = teste[0].unity1_fase1.title.toUpperCase();
-
-explicationP[0].textContent = teste[0].unity1_fase1.p1
-
-
-/* 
-------Tópico 1 ----------
-Choques
-
-O que são choques?
-Estado de Hipoperfusão Tecidual Sistêmica.
-Ou seja falta de oxigenação nos tecidos. 
-Caso o indivíduo não possua sangue, o mesmo terá choque, visto que, o oxigênio é encontrado no sangue através das células vermelhas.
-
-Saber mais
------ barra suspensa-----------------
-Entenda melhor como funciona a circulação sanguínea que leva o oxigênio para o corpo humano
-
---------video------------------
-https://youtu.be/lrzCIUTBgds?si=FdOtM1HCbTiZ1Zbe
-------------------------------
-Material complementar: 
-
-https://youtu.be/g1gW4coZL3I?si=6aQw48lVg04yc_zq;
-
-https://youtu.be/lrzCIUTBgds?si=FdOtM1HCbTiZ1Zbe
-*/
+// Unidade 1, fase 1: 
+imgHeader.src = content[0].unity1_fase1.imgHeader;
+titleHeader.textContent = content[0].unity1_fase1.title;
+explicationP[0].textContent = content[0].unity1_fase1.p1;
+separatorTitle[0].textContent = content[0].unity1_fase1.subtitle;
+explicationP[1].textContent = content[0].unity1_fase1.p2;
+mainVideo.src = content[0].unity1_fase1.video;
+mainUl.innerHTML = "<li>"+ content[0].unity1_fase1.li1 +  "</li>" + "<li>"+ content[0].unity1_fase1.li2 +  "</li>";
