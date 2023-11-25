61e49c6ad9bfa9d326f88bbe58a3cb7a54f9f8af
ojpbraga <ojpbraga@gmail.com>
2023-11-25 02:16:42 -0300

    fix, interaction and style: correções

--- frontend/js/question.js
+++ frontend/js/question.js
@@ -17,48 +17,24 @@ const btnEnviar = document.querySelector(".btn");
 const content = json;
 
 // Resultado do quiz: mensagem na tela
-const exibirResultado = document.getElementById("main-div-result");
-const backgroundImgResultado = document.getElementById("result-div-background");
-const imgResultado = document.getElementById("result-img-people");
-const textoResultado = document.getElementById("result-h1-phrase");
-const bntResultado = document.getElementById("result-bnt");
-
+const exibirAprovado = document.getElementById("main-div-result-approved");
+const exibirDesaprovado = document.getElementById("main-div-result-disapproved");
 
 console.log(content[unidade]);
 
+
 btnEnviar.addEventListener("click", async () => {
     //validação das respostas
-    const porcentagemResultado = 20; // Utilizar esta variável como resultado da porcentagem de acertos
+    const porcentagemResultado = 90; // Utilizar esta variável como resultado da porcentagem de acertos
 
-    // Aparição da resposta - é preciso a validação da resposta para funcionar corretamente (utlizar if).
-    // Obs.: pesquisar como bloquear a tela, quando essa parte estiver ativada.
-    // Acertou:
-    exibirResultado.style.display = "flex";
-    
-
-
-    // Errou:
-    backgroundImgResultado.style.background = "#FF4747";
-    imgResultado.setAttribute('src', './src/img/3d-casual-life-tired-female-student-sitting-with-book-on-her-face.png');
-    imgResultado.style.position = "absolute";
-    imgResultado.style.left = "15px";
-    textoResultado.style.fontSize = "1.875rem";
-    textoResultado.style.color = "#83C5BE";
-    textoResultado.innerHTML = 'Seu esforço foi<br>válido, falta pouco!<br>Acertos: <span id="result-h1-porcent">'+ porcentagemResultado +'%</span>';
-    const stylePorcentagem = document.getElementById("result-h1-porcent");
-    stylePorcentagem.style.fontSize = "1.875rem";
-    bntResultado.style.padding = "3vh 9vw";
-    bntResultado.style.fontSize = "1.7rem";
-    bntResultado.innerText = "Ver matérias";
-    exibirResultado.style.display = "flex";
-
-    // Se errou, div conteúdos para estudar:
+    // Aparição da resposta 
+    if (porcentagemResultado > 75) {
+        exibirAprovado.style.display = "flex";
+    } else if (porcentagemResultado < 75) {
+        exibirDesaprovado.style.display = "flex";
+    }
 
 
-
-
-    // Teste para saber se o botão está funcionando no celular
-    btnEnviar.style.background = "blue";
 
     //atualização do progresso
     //se passou na prova
