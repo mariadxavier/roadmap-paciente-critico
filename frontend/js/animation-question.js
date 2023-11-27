bb6aa72c473b055b618d410b8b291cfd59ce7cb6
ojpbraga <ojpbraga@gmail.com>
2023-11-27 16:34:49 -0300

    fix: responsividade

--- frontend/js/animation-question.js
+++ frontend/js/animation-question.js
@@ -1,7 +1,8 @@
 const setinha = document.getElementById("header-img-theme");
-// const header = document.querySelector("header");
+const header = document.querySelector("header");
 const imgHeader = document.getElementById("header-img-theme");
 const headerQuestion = document.getElementById("header-questions");
+console.log(header)
 
 window.addEventListener("scroll", function () {
     if (window.scrollY > 550) {
